8a337d1bf6478e69b3ba14cf8358f399ca1159fa
nitin7340 <108212936+nitin7340@users.noreply.github.com>
2022-08-25 02:24:30 +0530

    Add files via upload

---
+++ Assignment-23/file7.cpp
@@ -0,0 +1,11 @@
+//Write a C++ program to calculate the square of a number
+#include<iostream>
+using namespace std;
+int main(){
+    int x,y;
+    cout<<"Enter a number: ";
+    cin>>x;
+    y=x*x;
+    cout<<"Square of "<<x<<" is "<<y;
+    return 0;
+}
