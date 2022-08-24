2c6358107abd6635fbe76c020d751fa6eec42e02
nitin7340 <108212936+nitin7340@users.noreply.github.com>
2022-08-25 02:20:50 +0530

    Add files via upload

---
+++ Assignment-23/file3.cpp
@@ -0,0 +1,12 @@
+//Write a C++ program to calculate the sum of two numbers.
+
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b,s;
+    cout<<"Enter two numbers: ";
+    cin>>a>>b;
+    s=a+b;
+    cout<<"sum of "<<a<<" and "<<b<<" is "<<s;
+    return 0;
+}
